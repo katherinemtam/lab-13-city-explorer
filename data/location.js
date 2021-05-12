b8bfb0d4d998e55df98da25ec3db4865f6d42e81
Katherine Tam <katherinemtam@gmail.com>
2021-05-12 15:06:08 -0700

    add sample api data

---
+++ data/location.js
@@ -0,0 +1,58 @@
+export default [
+  {
+    place_id: '236905367',
+    licence: 'https://locationiq.com/attribution',
+    osm_type: 'node',
+    osm_id: '27564937',
+    boundingbox: [
+      '13.364834',
+      '13.684834',
+      '1.949823',
+      '2.269823'
+    ],
+    lat: '13.524834',
+    lon: '2.109823',
+    display_name: 'Niamey, 8001, Niger',
+    class: 'place',
+    type: 'city',
+    importance: 0.551650805597721,
+    icon: 'https://locationiq.org/static/images/mapicons/poi_place_city.p.20.png'
+  },
+  {
+    place_id: '62107078',
+    licence: 'https://locationiq.com/attribution',
+    osm_type: 'node',
+    osm_id: '5121728323',
+    boundingbox: [
+      '21.8223956',
+      '21.8623956',
+      '107.2284449',
+      '107.2684449'
+    ],
+    lat: '21.8423956',
+    lon: '107.2484449',
+    display_name: 'Name, Ningming County, Chongzuo City, Guangxi, China',
+    class: 'place',
+    type: 'village',
+    importance: 0.47500000000000003,
+    icon: 'https://locationiq.org/static/images/mapicons/poi_place_village.p.20.png'
+  },
+  {
+    place_id: '193722675',
+    licence: 'https://locationiq.com/attribution',
+    osm_type: 'way',
+    osm_id: '483606480',
+    boundingbox: [
+      '7.7504126',
+      '7.7504645',
+      '81.6453212',
+      '81.645347'
+    ],
+    lat: '7.7504645',
+    lon: '81.645347',
+    display_name: 'name, Batticaloa, Manmunai North DS Division, Batticaloa District, Eastern Province, 32300, Sri Lanka',
+    class: 'waterway',
+    type: 'drain',
+    importance: 0.43500000000000005
+  }
+];
